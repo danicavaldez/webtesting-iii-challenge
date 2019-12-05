53c39822f37bf98d0609f09b10563c4a47cd8fb4
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-12-04 21:06:17 -0800

    Almost finished with MVP

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,24 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react'
+import Controls from './Controls.js'
+
+test('Gate defaults to unlocked and open', () => {
+  const { getByText } = render(<Controls />)
+
+  getByText(/close gate/i)
+  getByText(/lock gate/i)
+})
+
+test('Gate cannot be closed or opened if it is locked', () => {
+  const { getByText } = render(<Controls locked={true} />)
+  
+  const toggleGate = jest.fn()
+
+  const button = getByText(/close gate/i)
+
+  fireEvent.click(button)
+
+  expect(toggleGate).not.toHaveBeenCalled()
+})
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,38 @@
 // Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+import Display from './Display.js';
+
+test('displays if gate is open/closed and if it locked/unlocked', () => {
+  const { getByText } = render(<Display />)
+
+  const openedGate = getByText(/open/i)
+  const unlockedGate = getByText(/unlocked/i)
+
+  expect(openedGate).toBeDefined()
+  expect(unlockedGate).toBeDefined()
+})
+
+
+test('displays Closed if the closed prop is true and Open if otherwise', () => {
+ 
+  const { getByText } = render(<Display />)
+
+  getByText(/open/i) || getByText(/closed/i)
+})
+
+test('displays Locked if the locked prop is true and Unlocked if otherwise', () => {
+
+  const { getByText } = render (<Display />)
+
+  getByText(/unlocked/i) || getByText(/locked/i)
+
+})
+
+test('when locked or closed use the red-led class', () => {
+  const { container } = render (<Display closed={true} locked ={true} />)
+  
+  const lockedAndClosed = container.querySelectorAll('.red-led')
+  expect(lockedAndClosed.length).toBe(2)
+
+})
